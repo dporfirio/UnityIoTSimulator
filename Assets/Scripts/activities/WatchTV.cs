249274a62c8e46670efe839c4501ad0bf72da094
David Porfirio <dporfirio@wisc.edu>
2020-12-07 23:06:14 -0600

    added new activity

---
+++ Assets/Scripts/activities/WatchTV.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class WatchTV : Activity
+{
+
+    // Start is called before the first frame update
+    public WatchTV(GameObject bed, Player player)
+    {
+    	this.actingObject = bed;
+        this.player = player;
+        this.command = "watch TV";
+        this.description = "watching TV";
+    }
+
+    public override bool CheckActivityConditions() {
+        bool isClose = this.actingObject.GetComponent<BedObject>().QueryPosition();
+        if (!isClose) {
+            this.EndAct();
+            return false;
+        }
+        else
+            return true;
+    }
+
+    public override void Act() {
+        this.player.UpdateActivity(this);
+    }
+
+    public override void EndAct() {
+
+    }
+}
