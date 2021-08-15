6536669597222c02466c2416912cb37e354c40a2
Chentao Wang <cwang556@wisc.edu>
2021-08-16 03:39:40 +0800

    Add start menu to enter id and select day

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public StartGame sg;
+    public void ChooseDay(int day)
+    {
+        // Send to ROS
+        sg.SendDate(day);
+        
+    }
+
+}
